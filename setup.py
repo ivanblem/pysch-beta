5728f6c668bd5f9437bedf557a9a1bd85df757a3
Ivan B <ivanblem@gmail.com>
2023-04-04 22:14:45 +0300

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pysch',
-    version='0.1',
+    version='0.1.1',
     description='',
     py_modules=['pysch'],
     package_dir={'pysch': 'pysch'},
